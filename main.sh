fcf392e021ceeddcc1ea373bf6365c3655dc6874
abdeldjalile-lafkir <lafkir.abdeldjalile@gmail.com>
2025-09-19 02:32:07 +0100

    Auto commit from local machine

--- main.sh
+++ main.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # Send the last commit to the VM after each commit
 VM_USER=lafkir_abdeldjalile
-VM_HOST=your.vm.ip
+VM_HOST=34.16.88.151
 VM_PATH=/home/lafkir_abdeldjalile/code
 
-git push origin main  # push commit to remote repo
-ssh $VM_USER@$VM_HOST "cd $VM_PATH && git pull origin main"
+git add .
+git commit -m "Auto commit from local machine"
+git push origin master
+
+
+
+
+ssh $VM_USER@$VM_HOST "cd $VM_PATH && git pull origin master
